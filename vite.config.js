67b49f5de8a14b9405f161b01e5817d08c9f96d2
Gabriel Gama <gabrielgamaalves1@gmail.com>
2026-04-25 16:36:18 -0300

    chore: initial project architecture

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+
+import path from "node:path"
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] })
+  ],
+    base: "/",
+  resolve: {
+    alias: {
+      "@": path.resolve(import.meta.dirname, "./src")
+    }
+  }
+})
