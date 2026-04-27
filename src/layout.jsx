fda6cb709dc3259a003f75778131b5d1247ba8f0
Gabriel Gama <gabrielgamaalves1@gmail.com>
2026-04-27 20:26:29 -0300

    feat: create header layout

--- src/layout.jsx
+++ src/layout.jsx
@@ -1,14 +1,16 @@
 "use client";
 
-import Sidebar from "./components/layout/Sidebar"
-import { useState } from "react";
+import Sidebar from "@/components/layout/Sidebar"
+import Header from "@/components/layout/Header";
+// import { useState } from "react";
 
 export default function Layout({ children }) {
   return (
     <div className="flex">
-      <aside className="h-screen p-3">
+      <aside className="h-screen">
         <Sidebar className="h-full" />
       </aside>
+      <Header />
       {children}
     </div>
   )
