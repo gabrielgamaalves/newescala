d768cb999712e1a9ed2984978d707f08514fee7d
Gabriel Gama <gabrielgamaalves1@gmail.com>
2026-04-27 23:26:53 -0300

    feat: flowbite removed

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
-import { ThemeInit } from "../.flowbite-react/init.js"
 
 import './globals.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeInit />
     <App />
   </StrictMode>,
 )
